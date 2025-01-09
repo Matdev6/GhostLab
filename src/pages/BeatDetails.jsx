ff13ab811bbc3ec063282ac9443b0f5165c399a6
Matdev6 <mateusousa123@hotmail.com>
2025-01-09 13:42:28 -0300

    ajuste BeatDatails
    ajuste BeatDatails

--- src/pages/BeatDetails.jsx
+++ src/pages/BeatDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { beats } from '../components/beatsData'; // Certifique-se de que o caminho está correto
+import { Link } from 'react-router-dom';
 
 const BeatDetails = () => {
   const { name } = useParams(); // Captura o parâmetro "name" da URL
@@ -10,11 +11,12 @@ const BeatDetails = () => {
   }
 
   return (
-    <div className="text-white p-4 bg-black h-screen w-screen">
-        <div>
-      <h1 className="text-3xl">{beat.name}</h1>
-      <img src={beat.img} alt={beat.name} className="w-96 mx-auto my-4" />
-      <p>{beat.name}</p>
+    <div className="text-white p-4 bg-black h-screen w-screen flex">
+      <div className='m-auto'>
+        <h1 className="text-3xl">{beat.name}</h1>
+        <img src={beat.img} alt={beat.name} className="w-96 mx-auto my-4" />
+        <p>{beat.name}</p>
+        <button className='text-white flex justify-center items-center p-4 bg-roxo-claro hover:bg-roxo-escuro transition-all w-48 h-10 rounded-lg mt-4 mx-auto font-bold'><Link className='px-16 py-2' to={'/home'}>Voltar</Link></button>
       </div>
     </div>
   );
